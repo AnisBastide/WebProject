95d4c99286361c7e0aed49a2fc4365b55f5bf41d
Julien2455 <julien.viseux@edu.itescia.fr>
2020-06-08 16:02:44 +0200

    Init web project

---
+++ ProjectWeb/plugin/header.php
@@ -0,0 +1,22 @@
+<header>
+
+<nav>
+
+<ul>
+    <li><a href="https://playoverwatch.com/fr-fr/"><img src="img/ow.png" height="55px"></a></li>
+    <li><a class='hvr-bob' href="index.html">MENU</a></li>
+    <li><a class='hvr-bob' href="characters.html">CHARACTERS</a></li>
+    <li><a class='hvr-bob' href="maps.html">MAPS</a></li>
+    <li><a class='hvr-bob' href="events.html">EVENTS</a></li>
+    <li><a class='hvr-bob' href="overwatch_league.html">OVERWATCH LEAGUE</a></li>
+    <li><a class='hvr-bob' href="shop.html">SHOP</a></li>
+    <li><a class='hvr-bob' href="connection.html">CONNECTION</a></li>
+    <li><a class='hvr-bob' id="buy" href="https://shop.blizzard.com/product/overwatch?blzcmp=ow_buy_nav">BUY
+            OVERWATCH©</a></li>
+</ul>
+
+
+
+
+</nav>
+</header>
