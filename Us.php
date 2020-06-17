9525485f31890d5aeaa420cf25e6d05aed04ea34
anis bastide <anis.bastide@upslide.net>
2020-06-17 16:45:41 +0200

    added itescia's text and anis text

--- Us.php
+++ Us.php
@@ -15,43 +15,10 @@
             <div class="us_row">
                 <div><img class="imgborder" src="img/itescia.png"></div>
                 &emsp;&emsp;
-                <div class="txt">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Nec sagittis aliquam malesuada
-                    bibendum. Elementum nibh tellus molestie nunc. Tempus urna et pharetra pharetra.
-                    Aliquam sem et tortor consequat id porta nibh venenatis. Lacinia at quis risus
-                    sed. Urna condimentum mattis pellentesque id nibh tortor id. Ut sem viverra aliquet
-                    eget sit amet. Tellus id interdum velit laoreet id donec ultrices tincidunt arcu.
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames. At
-                    tempor commodo ullamcorper a lacus vestibulum sed. Lobortis feugiat vivamus at
-                    augue eget arcu. Laoreet suspendisse interdum consectetur libero id faucibus nisl
-                    tincidunt. Blandit aliquam etiam erat velit scelerisque in. Donec ultrices tincidunt
-                    arcu non. Tellus mauris a diam maecenas sed enim ut sem viverra. Nulla facilisi cras
-                    fermentum odio eu feugiat pretium nibh ipsum. Elementum tempus egestas sed sed risus.
-                    Nisi scelerisque eu ultrices vitae auctor eu augue.
-
-                    Porta lorem mollis aliquam ut porttitor. Dictum non consectetur a erat nam at lectus urna
-                    . Ac tortor dignissim convallis aenean et tortor at risus viverra. Ac placerat vestibulum
-                    lectus mauris ultrices. Elit ullamcorper dignissim cras tincidunt. Aenean vel elit scelerisque
-                    mauris pellentesque pulvinar pellentesque habitant. Ipsum nunc aliquet bibendum enim
-                    facilisis gravida neque. Sit amet porttitor eget dolor morbi non arcu risus. Dictumst
-                    vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt.
-                    Vel eros donec ac odio tempor orci dapibus. Egestas purus viverra accumsan in nisl
-                    nisi. Quisque egestas diam in arcu cursus euismod. Urna duis convallis convallis
-                    tellus id interdum velit. Nibh cras pulvinar mattis nunc sed blandit.
-
-                    Et magnis dis parturient montes. Aenean pharetra magna ac placerat vestibulum
-                    lectus mauris ultrices. Nisi porta lorem mollis aliquam ut porttitor leo a.
-                    Imperdiet nulla malesuada pellentesque elit eget. Morbi blandit cursus risus
-                    at ultrices. Tincidunt praesent semper feugiat nibh sed pulvinar. Posuere
-                    morbi leo urna molestie. Facilisi etiam dignissim diam quis enim lobortis.
-                    Quam adipiscing vitae proin sagittis nisl rhoncus. Ut consequat semper
-                    viverra nam libero justo laoreet sit. Massa enim nec dui nunc mattis
-                    enim ut tellus. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus.
-                    Mi eget mauris pharetra et ultrices neque ornare aenean. Tristique et egestas
-                    quis ipsum suspendisse ultrices. Vitae justo eget magna fermentum iaculis eu.
-                    Enim blandit volutpat maecenas volutpat blandit aliquam. Eget mauris pharetra et ultrices
-                    neque ornare. Laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt.
-                    Aenean euismod elementum nisi quis. In nisl nisi scelerisque eu ultrices.</div>
+                <div class="txt">Itescia is the school of I-management, it offers many diplomas in these fields such as the computer development diploma or the diploma of human resources. 
+                    It offers apprenticeship training. This allows you to learn in business while being paid and also allows you not to pay the cost of your degree.
+                    Itescia has several campuses, one in Cergy and one in Cergy-Pontoise. The school was founded in 2004,
+                    it was born from the merger of ITIN (an alternating computer school) and ESCIA (a school of management and management).</div>
             </div>
         </div>
         </br>
@@ -131,15 +98,11 @@
                 </div>
 
                 <h3>Anis</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Facilisis mauris sit amet massa vitae tortor. Nisl
-                    condimentum id venenatis a condimentum vitae. Non enim
-                    praesent elementum facilisis leo. Nibh tortor id aliquet
-                    lectus proin nibh nisl. Egestas integer eget aliquet
-                    nibh praesent tristique magna. Iaculis eu non diam phasellus
-                    vestibulum lorem sed risus. Augue neque gravida in fermentum
-                    et sollicitudin ac. </p>
+                <p>Hello, I'm Anis.
+                    I am an apprentice developer at the coding factory, I work in a company that uses c# and vb .net.
+                    I'm a back developer, I prefer to take care of what's going on behind and not the visual part of the projects.
+                    My passions are code, video games and music.
+                    Later I would like to be a developer at blizzard Entertainment.</p>
             </div>
         </div>
 
